058a91385adaf88d8298173a72c354863d92f964
Ixtlan <ixtlan@gmail.com>
2025-09-20 12:24:47 +0200

    Bugfix sql

--- database/migrations/1750872682__view_ticker_tdx.sql
+++ database/migrations/1750872682__view_ticker_tdx.sql
@@ -2,5 +2,6 @@ create view stockmarket.v_ticker_dx as
 select t.ticker, max(s.dx) from stockmarket.stockprice s 
 join stockmarket.stocktickers t on t.oid = s.ticker_id
 where t.status = 1
+group by t.ticker;
 
 insert into stockmarket.migrations (version,comment) values (1750872682,'View ticker tdx');
