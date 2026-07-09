ba788c079c07da500eb77f45260ac641c076ceae
Ixtlan <ixtlan@gmail.com>
2026-07-09 14:32:44 +0200

    WIP

--- database/migrations/inserts/postgres/1783598637__stockmarket_dev_test_data.sql
+++ database/migrations/inserts/postgres/1783598637__stockmarket_dev_test_data.sql
@@ -7,6 +7,9 @@ insert into stockmarket.optionx (opname,strike,exp_date,optype,stock_id,series) 
 -- optionpurchase
 -- oid | opid  |     dx     | price | volume | status | transaction_cost | purchase_type | spot  | buy
 --  47 | 26979 | 2019-02-14 |  5.80 |     10 |      1 |             0.00 |            11 | 34.70 | 5.10
+-- spot 437.70
+-- call settl: 29.24, bid: 20.00, ask: 22.75
+-- put settl: 15.11, bid: 20.75, ask: 23.75
 
 -- critter
 -- oid | status | critter_type | sell_vol | purchase_id | sale_id
