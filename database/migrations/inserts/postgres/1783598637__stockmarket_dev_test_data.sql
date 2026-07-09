93cb7c92c57e097a6d85825f0e141838ea39430c
Ixtlan <ixtlan@gmail.com>
2026-07-09 14:23:44 +0200

    WIP

---
+++ database/migrations/inserts/postgres/1783598637__stockmarket_dev_test_data.sql
@@ -0,0 +1,19 @@
+--- new migration 2026-07-09 ---
+
+insert into stockmarket.optionx (opname,strike,exp_date,optype,stock_id,series) values ('YAR6H440',440.00,'2026-08-21','c',3,'6H');
+
+-- 26980
+
+-- optionpurchase
+-- oid | opid  |     dx     | price | volume | status | transaction_cost | purchase_type | spot  | buy
+--  47 | 26979 | 2019-02-14 |  5.80 |     10 |      1 |             0.00 |            11 | 34.70 | 5.10
+
+-- critter
+-- oid | status | critter_type | sell_vol | purchase_id | sale_id
+--  45 |      7 |            1 |       10 |          47 |
+
+-- accept_rules
+-- oid | value | rtyp | active | description | cid
+-- 72 |  3.00 |    1 | y      |             |  45
+
+-- insert into art.migrations (version,comment) values (1783598637,'Stockmarket dev test data');
